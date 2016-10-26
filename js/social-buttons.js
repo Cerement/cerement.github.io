c12beb18aac9dc40515b08d8005bfc7fa0fb51bd
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-10-26 20:26:37 +0800

    A whole new site yay

---
+++ js/social-buttons.js
@@ -0,0 +1,153 @@
+$(document).ready(function() {
+  $('i').hide();
+})
+
+$(window).load(function() {
+  $('i').show();
+
+  var twitterPos = $('#twitter').position();
+  var githubPos = $('#github').position();
+  //var stackPos = $('#stack').position();
+  //var linkedinPos = $('#linkedin').position();
+  //var codePos = $('#code').position();
+  //var plusPos = $('#plus').position();
+  var mailPos = $('#mail').position();
+  
+  /*
+  $('i').css({
+    position: 'absolute',
+    zIndex: '1',
+    //top: '10%',
+    left: '40%' 
+  });
+  */
+
+  $('#twitter').css({
+    position: 'absolute',
+    zIndex: '1',
+    top: twitterPos.top - 100,
+    left: twitterPos.left 
+  });
+
+  $('#github').css({
+    position: 'absolute',
+    zIndex: '1',
+    top: githubPos.top - 100,
+    left: githubPos.left 
+  });
+
+  $('#mail').css({
+    position: 'absolute',
+    zIndex: '1',
+    top: mailPos.top - 100,
+    left: mailPos.left 
+  });
+  
+  setTimeout(function() {
+    $('#twitter').animate({
+      top: twitterPos.top + 10,
+      left: twitterPos.left
+    }, 500);
+  }, 250);
+  
+  setTimeout(function() {
+    $('#twitter').animate({
+      top: twitterPos.top,
+      left: twitterPos.left
+    }, 250);
+    
+    $('#github').animate({
+      top: githubPos.top + 10,
+      left: githubPos.left
+    }, 500);
+  }, 500);
+
+  setTimeout(function() {
+    $('#github').animate({
+      top: githubPos.top,
+      left: githubPos.left
+    }, 250);
+    
+    $('#mail').animate({
+      top: mailPos.top + 10,
+      left: mailPos.left
+    }, 500);
+  }, 750);
+
+  setTimeout(function() {
+    $('#mail').animate({
+      top: mailPos.top,
+      left: mailPos.left
+    }, 250);
+  }, 1000);
+  
+  /*
+  setTimeout(function() {
+    $('#github').animate({
+      top: githubPos.top,
+      left: githubPos.left
+    }, 250);
+    
+    $('#stack').animate({
+      top: stackPos.top + 10,
+      left: stackPos.left - 3
+    }, 500);
+  }, 750);
+  
+  setTimeout(function() {
+    $('#stack').animate({
+      top: stackPos.top,
+      left: stackPos.left
+    }, 250);
+    
+    $('#linkedin').animate({
+      top: linkedinPos.top + 10,
+      left: linkedinPos.left
+    }, 500);
+  }, 1000);
+  
+  setTimeout(function() {
+    $('#linkedin').animate({
+      top: linkedinPos.top,
+      left: linkedinPos.left
+    }, 250);
+    
+    $('#code').animate({
+      top: codePos.top + 10,
+      left: codePos.left + 3
+    }, 500);
+  }, 1250);
+  
+  setTimeout(function() {
+    $('#code').animate({
+      top: codePos.top,
+      left: codePos.left
+    }, 250);
+    
+    $('#plus').animate({
+      top: plusPos.top + 10,
+      left: plusPos.left + 6
+    }, 500);
+  }, 1500);
+  
+  setTimeout(function() {
+    $('#plus').animate({
+      top: plusPos.top,
+      left: plusPos.left
+    }, 250);
+    
+    $('#mail').animate({
+      top: mailPos.top + 10,
+      left: mailPos.left + 10
+    }, 500);
+  }, 1750);
+  
+  setTimeout(function() {
+    $('#mail').animate({
+      top: mailPos.top,
+      left: mailPos.left
+    }, 250);
+  }, 2000);
+  */
+  
+})
